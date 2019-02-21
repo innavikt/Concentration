decbbbaefe0ae58dab336d190339d409a41a4a94
inna <inna@innas-iMac.local>
2019-02-21 07:48:45 -0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by inna on 21/02/2019.
+//  Copyright © 2019 inna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
